40baca5a33a5db169285512cb06c8bc9dc2a0267
sergioramirezmoron <sergiothd50@gmail.com>
2025-09-25 18:00:21 +0200

    updated script with jq

--- Ejemplo1/scripts/peticiones_http.sh
+++ Ejemplo1/scripts/peticiones_http.sh
@@ -1,11 +1,11 @@
 echo "--------------GET--------------"
-curl -X GET http://localhost:3000/books/3
+curl -X GET http://localhost:3000/books/3 | jq .
 
 echo "\n--------------POST--------------"
-curl -X POST http://localhost:3000/books -H "Content-Type: application/json" -d '{"title": "New Book", "author": "Author Name"}'
+curl -X POST http://localhost:3000/books -H "Content-Type: application/json" -d '{"title": "New Book", "author": "Author Name"}' | jq .
 
 echo "\n--------------PUT--------------"
-curl -X PUT http://localhost:3000/books/1 -H "Content-Type: application/json" -d '{"title": "Updated Book", "author": "Updated Author"}'
+curl -X PUT http://localhost:3000/books/1 -H "Content-Type: application/json" -d '{"title": "Updated Book", "author": "Updated Author"}' | jq .
 
 echo "\n--------------DELETE--------------"
-curl -X DELETE http://localhost:3000/books/3
+curl -X DELETE http://localhost:3000/books/3 | jq .
