fbf7269652e7398f9851d7e2e2d2bb294d1b27fe
sergioramirezmoron <sergiothd50@gmail.com>
2025-10-02 14:35:32 +0200

    Comment functions

--- manual-http-SergioRamirez-RitaVicente/src/crud-curl.js
+++ manual-http-SergioRamirez-RitaVicente/src/crud-curl.js
@@ -4,9 +4,46 @@ dotenv.config();
 const baseUrl = `${process.env.API_BASE_URL}/${process.env.PORT}`;
 console.log(baseUrl);
 
-const createStudent = (studentData) => {};
+/**
+ * Create an student with an object
+ * @autor - Sergio Ramírez & Rita Vicente
+ * @param {Object} studentData - Student that we want to create
+ * @return - A curl command to create a new student
+ */
+const createStudent = (studentData = {}) => {};
+/**
+ * Read all students
+ * @autor - Sergio Ramírez & Rita Vicente
+ * @return - A curl command with all students
+ */
 const readAllStudents = () => {};
-const readStudentById = (id) => {};
-const updateStudent = (id, studentData) => {};
-const patchStudent = (id, partialData) => {};
-const deleteStudent = (id) => {};
+/**
+ * Read a student with the id
+ * @autor - Sergio Ramírez & Rita Vicente
+ * @param {Number} id - Student id that we want to read
+ * @return - A curl command to read a student
+ */
+const readStudentById = (id = 1) => {};
+/**
+ * Update an student with an object and id
+ * @autor - Sergio Ramírez & Rita Vicente
+ * @param {Number} id - Student that we want to update
+ * @param {Object} studentData - Student that we want to update
+ * @return - A curl command to update a student
+ */
+const updateStudent = (id = 1, studentData = {}) => {};
+/**
+ * Patch a student with an object and id
+ * @autor - Sergio Ramírez & Rita Vicente
+ * @param {Number} id - Student that we want to patch
+ * @param {Object} partialData - Student that we want to patch
+ * @return - A curl command to patch a student
+ */
+const patchStudent = (id = 1, partialData = {}) => {};
+/**
+ * Delete a student with an object
+ * @autor - Sergio Ramírez & Rita Vicente
+ * @param {Number} id - Student that we want to delete
+ * @return - A curl command to delete a student
+ */
+const deleteStudent = (id = 1) => {};
